c0c5d750069bed6f82207275a5b6d195063bb14c
Thet Htoo Aung <ththtooaung.e@gmail.com>
2024-03-12 16:53:19 +0630

    view file integration

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ArtistController;
+use App\Http\Controllers\ItemController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/item-export', [ItemController::class, 'export'])->name('item-export');
+Route::get('/artist-export', [ArtistController::class, 'export'])->name('artist-export');
+
+Route::post('/item-import', [ItemController::class, 'import'])->name('item-import');
+Route::post('/artist-import', [ArtistController::class, 'import'])->name('artist-import');
